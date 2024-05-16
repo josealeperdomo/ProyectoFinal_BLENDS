892b2a722e3570bdf993e317912b6d470111a51b
Jose Perdomo <perdomomolinajose@gmail.com>
2024-05-15 22:48:05 -0400

    VER PAGOS Y FACTURA

--- backend/app/routes/pagos.js
+++ backend/app/routes/pagos.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const {
+  verPagos,
   insertarPago,
   aceptarPago,
   rechazarPago
 } = require('../controllers/pagos');
 
 // Rutas para insertar, aceptar y rechazar pagos
+router.get('/', verPagos);
 router.post('/pagos', insertarPago);
-router.put('/pagos/:id/aceptar', aceptarPago);
+router.patch('/pagos/:id/aceptar', aceptarPago);
 router.delete('/pagos/:id/rechazar', rechazarPago);
 
 module.exports = router;
