1ed25d099119fe9d6d8e1340bae8c7d6e85eb701
scarlhs26 <lycrek@gmail.com>
2024-05-20 18:57:23 -0400

    Interfaz principal Componentes principales

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { Login } from './pages/Login.jsx';
 import { Register } from './pages/Register.jsx';
+import { Feed } from './pages/Feed.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/Home" element={<Feed />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,7 +1,40 @@
 import "../styles/General.css";
+import "../styles/Feed.css"
+import NavArriba from "../components/NavArriba"; 
+import NavIzq from "../components/NavIzq";
+import NavDer from "../components/NavDer";
 export function Feed(){
     return(
         <>
+            <div className="home">
+        {/*------------------------ SECCION SUPERIOR---------------*/}
+        <NavArriba />
+
+        <section className="seccion-principal">
+
+          {/*------------------------ SECCION LATERAL IZQUIERDA-------------------*/}
+          <div className="lateral-izquierda"><NavIzq /></div>
+          
+          {/*------------------------ SECCION CENTRO API------------------------*/}
+          <section className="central-opciones">
+            <div className="seccion-general">
+              <div className="texto-home">
+              <h1>Lorem ipsum dolor sit.</h1>
+              <p className="texto-p-home">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
+                nesciunt veniam iure dignissimos itaque consequuntur earum odio
+                perferendis cumque, accusamus maxime, iste pariatur rem quia
+                provident quaerat praesentium quae. Consequuntur.
+                <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+              </p>
+            </div>
+            </div>
+          </section>
+          {/*------------------------ SECCION LATERAL DERECHA---------------*/}
+          <div className="lateral-derecha"> <NavDer /> </div>
+          
+        </section>
+      </div>
         </>
     )
 }
