1f720fbe20d720f355f09e9cdf2449d037ae3716
scarlhs26 <lycrek@gmail.com>
2024-05-25 16:56:48 -0400

    funcionabilidad Likes

--- frontend/src/components/Publicaciones.jsx
+++ frontend/src/components/Publicaciones.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
+import { Link } from 'react-router-dom';
 import '../styles/Feed.css';
 import defaultUserImage from '../img/img-user.png';
 
@@ -27,45 +27,43 @@ const Publicaciones = () => {
 
     return (
         <div> 
-            {publicaciones.map((publicacion) => (
-
-                    <div className="row border-radius">
-                        <div className="feed">
-                            <div className="feed_title">
-                                <img src={publicacion.usuario_publicacion ? publicacion.usuario_publicacion.imagen_perfil : defaultUserImage} alt="Imagen de usuario" />
-                                <span>
-                                    <b>{publicacion.usuario_publicacion ? publicacion.usuario_publicacion.usuario : 'Usuario Desconocido'}</b> hizo una <Link to={`/publicacion/${publicacion._id}`} key={publicacion._id}> <a href="#">Publicacion</a></Link>
-                                    <p>{new Date(publicacion.createdAt).toLocaleString()}</p>
-                                </span>
+            {publicaciones.map((publicacion, index) => (
+                <div className="row border-radius" key={index}>
+                    <div className="feed">
+                        <div className="feed_title">
+                            <img src={publicacion.usuario_publicacion ? publicacion.usuario_publicacion.imagen_perfil : defaultUserImage} alt="Imagen de usuario" />
+                            <span>
+                                <b>{publicacion.usuario_publicacion ? publicacion.usuario_publicacion.usuario : 'Usuario Desconocido'}</b> hizo una <Link to={`/publicacion/${publicacion._id}`}>Publicación</Link>
+                                <p>{new Date(publicacion.createdAt).toLocaleString()}</p>
+                            </span>
+                        </div>
+                        <div className="feed_content">
+                            <div className="feed_content_image">
+                                <p>{publicacion.texto}</p>
                             </div>
-                            <div className="feed_content">
+                            {publicacion.imagen_publicacion && (
                                 <div className="feed_content_image">
-                                    <p>{publicacion.texto}</p>
+                                    <img src={publicacion.imagen_publicacion} alt="Imagen de la publicación" />
                                 </div>
-                                {publicacion.imagen_publicacion && (
-                                    <div className="feed_content_image">
-                                        <img src={publicacion.imagen_publicacion} alt="Imagen de la publicación" />
-                                    </div>
-                                )}
-                            </div>
-                            <div className="feed_footer">
-                                <ul className="feed_footer_left">
-                                    <li className="hover-orange selected-orange"><i className="fa fa-heart"></i> 22k</li>
-                                    <li><span><b>Jimmy, Andrea</b> and 47 more liked this</span></li>
-                                </ul>
-                                <ul className="feed_footer_right">
-                                    <div>
-                                        <li className="hover-orange selected-orange"><i className="fa fa-share"></i> 7k</li>
-                                        <li className="hover-orange"><i className="fa fa-comments-o"></i> 860 comments</li>
-                                    </div>
-                                </ul>
-                            </div>
+                            )}
+                        </div>
+                        <div className="feed_footer">
+                            <ul className="feed_footer_left">
+                                <li className="hover-orange selected-orange"><i className="fa fa-heart"></i> 22k</li>
+                                <li><span><b>Jimmy, Andrea</b> and 47 more liked this</span></li>
+                            </ul>
+                            <ul className="feed_footer_right">
+                                <div>
+                                    <li className="hover-orange selected-orange"><i className="fa fa-share"></i> 7k</li>
+                                    <li className="hover-orange"><i className="fa fa-comments-o"></i> 860 comments</li>
+                                </div>
+                            </ul>
                         </div>
                     </div>
+                </div>
             ))}
         </div>
     );
 };
 
 export default Publicaciones;
-
--- frontend/src/pages/PublicacionPost.jsx
+++ frontend/src/pages/PublicacionPost.jsx
@@ -12,24 +12,26 @@ export function PublicacionPost() {
   const [publicacion, setPublicacion] = useState(null);
   const [comentario, setComentario] = useState('');
   const [comentarios, setComentarios] = useState([]);
+  const [likes, setLikes] = useState(0); 
   const [error, setError] = useState(null);
+
+  const fetchComentarios = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/comentarios/publicaciones/${id}`);
+      setComentarios(response.data);
+    } catch (error) {
+      console.error('Error al obtener los comentarios:', error);
+    }
+  };
 
   useEffect(() => {
     const fetchPublicacion = async () => {
       try {
         const response = await axios.get(`http://localhost:3000/publicaciones/${id}`);
         setPublicacion(response.data);
+        setLikes(response.data.likes);
       } catch (error) {
         console.error('Error al obtener los detalles de la publicación:', error);
-      }
-    };
-
-    const fetchComentarios = async () => {
-      try {
-        const response = await axios.get(`http://localhost:3000/comentarios/publicaciones/${id}`);
-        setComentarios(response.data);
-      } catch (error) {
-        console.error('Error al obtener los comentarios:', error);
       }
     };
 
@@ -46,8 +48,12 @@ export function PublicacionPost() {
         return;
       }
 
-      await axios.post(`http://localhost:3000/comentarios/publicaciones`, {
+      const decodedToken = JSON.parse(atob(token.split('.')[1]));
+      const usuario_comentario = decodedToken.id;
+
+      const response = await axios.post(`http://localhost:3000/comentarios/publicaciones`, {
         id_Publicacion: id,
+        usuario_comentario,
         texto: comentario,
       }, {
         headers: {
@@ -55,6 +61,7 @@ export function PublicacionPost() {
         }
       });
 
+  
       fetchComentarios();
 
       setComentario('');
@@ -64,106 +71,175 @@ export function PublicacionPost() {
     }
   };
 
+  const handleLike = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        console.error('Usuario no autenticado');
+        return;
+      }
+  
+      const decodedToken = JSON.parse(atob(token.split('.')[1]));
+      const id_usuario = decodedToken.id;
+  
+      const response = await axios.get(`http://localhost:3000/likes/publicaciones/${id}/usuario/${id_usuario}/like`);
+      const liked = response.data.liked;
+  
+      if (liked) {
+        // Si el usuario ya dio like, quitar el like
+        await quitarLike(id);
+        setLikes(likes - 1);
+      } else {
+        // Si el usuario no ha dado like, dar el like
+        await darLike(id);
+        setLikes(likes + 1);
+      }
+    } catch (error) {
+      console.error('Error al manejar el like:', error);
+      setError('Error al manejar el like. Por favor, inténtalo de nuevo.');
+    }
+  };
+  
+  const darLike = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const decodedToken = JSON.parse(atob(token.split('.')[1]));
+      const id_usuario = decodedToken.id;
+  
+      await axios.post(`http://localhost:3000/likes/publicaciones/${id}/like`, {
+        id_usuario: id_usuario
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+    } catch (error) {
+      throw new Error('Error al dar like: ' + error.message);
+    }
+  };
+  
+  const quitarLike = async (id) => {
+    try {
+      const token = localStorage.getItem('token');
+      const decodedToken = JSON.parse(atob(token.split('.')[1]));
+      const id_usuario = decodedToken.id;
+  
+      await axios.delete(`http://localhost:3000/likes/publicaciones/${id}/unlike/${id_usuario}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+
+      });
+      console.log('Like quitado');
+    } catch (error) {
+      throw new Error('Error al quitar like: ' + error.message);
+    }
+  };
+  
+  
+
   if (!publicacion) {
-  return <div>Cargando...</div>;
+    return <div>Cargando...</div>;
   }
 
   return (
-    <>
-      <div className="home">
-        <div className="lateral-derecha">
-          <NavDer />
+    <div className="home">
+      <div className="lateral-derecha">
+        <NavDer />
+      </div>
+      <NavArriba />
+      <section className="seccion-principal">
+        <div className="lateral-izquierda">
+          <NavIzq />
         </div>
-        <NavArriba />
-        <section className="seccion-principal">
-          <div className="lateral-izquierda">
-            <NavIzq />
-          </div>
-          <section className="central-opciones">
-            <div className="seccion-general">
-              <a className="row salir-post" onClick={() => window.history.back()}>
-                <div className="fa fa-arrow-left"></div>
-                <h3>Salir del post</h3>
-              </a>
-              <div className="row border-radius">
-                <div className="feed">
-                  <div className="feed_title">
-                    <img src={publicacion.usuario_publicacion.imagen_perfil} alt="" />
-                    <span>
-                      <b>{publicacion.usuario_publicacion.usuario}</b> hizo una{" "}
-                      <a href="#">Publicacion</a>
-                      <p>{new Date(publicacion.createdAt).toLocaleString()}</p>
-                    </span>
+        <section className="central-opciones">
+          <div className="seccion-general">
+            <a className="row salir-post" onClick={() => window.history.back()}>
+              <div className="fa fa-arrow-left"></div>
+              <h3>Salir del post</h3>
+            </a>
+            <div className="row border-radius">
+              <div className="feed">
+                <div className="feed_title">
+                  <img src={publicacion.usuario_publicacion.imagen_perfil} alt="" />
+                  <span>
+                    <b>{publicacion.usuario_publicacion.usuario}</b> hizo una{" "}
+                    <a href="#">Publicacion</a>
+                    <p>{new Date(publicacion.createdAt).toLocaleString()}</p>
+                  </span>
+                </div>
+                <div className="feed_content">
+                  <div className="feed_content_image">
+                    <p>{publicacion.texto}</p>
                   </div>
-                  <div className="feed_content">
-                    <div className="feed_content_image">
-                      <p>{publicacion.texto}</p>
-                    </div>
-                    <div className="feed_content_image">
-                      <img src={publicacion.imagen_publicacion} alt="" />
-                    </div>
-                  </div>
-                  <div className="feed_footer">
-                    <ul className="feed_footer_left">
-                      <li className="hover-orange selected-orange">
-                        <i className="fa fa-heart"></i> 22k
-                      </li>
-                      <li>
-                        <span>
-                          <b>Jimmy, Andrea</b> and 47 more liked this
-                        </span>
-                      </li>
-                    </ul>
-                    <ul className="feed_footer_right">
-                      <div>
-                        <li className="hover-orange selected-orange">
-                          <i className="fa fa-share"></i> 7k
-                        </li>
-                        <li className="hover-orange">
-                          <i className="fa fa-comments-o"></i> {comentarios.length} comments
-                        </li>
-                      </div>
-                    </ul>
+                  <div className="feed_content_image">
+                    <img src={publicacion.imagen_publicacion} alt="" />
                   </div>
                 </div>
-                <div className="feedcomments">
-                  <ul>
-                    {comentarios.map(comentario => (
-                      <li key={comentario._id}>
-                        <div className="feedcomments-user">
-                          <img src={comentario.usuario_comentario.imagen_perfil} alt="" />
-                          <span>
-                            <b>{comentario.usuario_comentario.usuario}</b>
-                            <p>{new Date(comentario.fecha_creacion).toLocaleString()}</p>
-                          </span>
-                        </div>
-                        <div className="feedcomments-comment">
-                          <p>{comentario.texto}</p>
-                        </div>
-                        {/* Otros elementos del comentario */}
+                <div className="feed_footer">
+                  <ul className="feed_footer_left">
+                    <li className="hover-orange selected-orange">
+                      <i className="fa fa-heart" onClick={handleLike}></i> {likes} Likes
+                    </li>
+                    <li>
+                      <span>
+                        <b>Jimmy, Andrea</b> y 47 más les gustó esto
+                      </span>
+                    </li>
+                  </ul>
+                  <ul className="feed_footer_right">
+                    <div>
+                      <li className="hover-orange selected-orange">
+                        <i className="fa fa-share"></i> 7k
                       </li>
-                    ))}
+                      <li className="hover-orange">
+                        <i className="fa fa-comments-o"></i> {comentarios.length} comentarios
+                      </li>
+                    </div>
                   </ul>
                 </div>
-                <div className="textarea-comentarios">
-                  <img src="images/user.jpg" alt="Imagen de usuario" />
-                  <form onSubmit={handleSubmitComentario}>
-                      <input type="text"
-                      name=""
-                      id=""
-                      placeholder="Comentar"
-                      value={comentario}
-                      onChange={(e) => setComentario(e.target.value)} />
-                    <button type="submit" className="fa fa-send"></button>
-                  </form>
-                  {error && <div className="error-message">{error}</div>}
-                </div>
+              </div>
+              <div className="feedcomments">
+                <ul>
+                  {comentarios.map(comentario => (
+                    <li key={comentario._id}>
+                      <div className="feedcomments-user">
+                        <img src={comentario.usuario_comentario.imagen_perfil} alt="" />
+                        <span>
+                          <b>{comentario.usuario_comentario.usuario}</b>
+                          <p>{new Date(comentario.fecha_creacion).toLocaleString()}</p>
+                        </span>
+                      </div>
+                      <div className="feedcomments-comment">
+                        <p>{comentario.texto}</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="textarea-comentarios">
+                <img src="images/user.jpg" alt="Imagen de usuario"/>
+                <form onSubmit={handleSubmitComentario}>
+                  <input
+                    type="text"
+                    placeholder="Comentar"
+                    value={comentario}
+                    onChange={(e) => setComentario(e.target.value)}
+                  />
+                  <button type="submit" className="fa fa-send"></button>
+                </form>
+                {error && <div className="error-message">{error}</div>}
               </div>
             </div>
-          </section>
+          </div>
         </section>
-      </div>
-    </>
+      </section>
+    </div>
   );
 }
+
+
+
+
+
 
