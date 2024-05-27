d28f33acec3b4ac4dd9af8adc886c25590e54fb2
scarlhs26 <lycrek@gmail.com>
2024-05-27 00:13:06 -0400

    ConfigAdmin

---
+++ frontend/src/pages/PagoMembresiaAdmin.jsx
@@ -0,0 +1,87 @@
+import "../styles/General.css";
+import "../styles/Feed.css";
+import NavArriba from "../components/NavArriba";
+import NavIzq from "../components/NavIzq";
+import NavDer from "../components/NavDer";
+import fotoejemplo from "../img/photo-1.jpg";
+import { Link } from "react-router-dom";
+export function PagoMembresiaAdmin() {
+  return (
+    <>
+      <div className="home">
+        {/*------------------------ SECCION SUPERIOR---------------*/}
+        <NavArriba />
+
+        <section className="seccion-principal">
+          {/*------------------------ SECCION LATERAL IZQUIERDA-------------------*/}
+          <div className="lateral-izquierda">
+            <NavIzq />
+          </div>
+
+          {/*------------------------ SECCION CENTRO API------------------------*/}
+          <section className="central-opciones">
+            <div className="seccion-general">
+              <div className="row amigos ">
+                <div className="row shadow config-menu">
+                  <div className="row_title">
+                    <span>Administracion del admin</span>
+                  </div>
+                  <div className="menusetting_contain">
+                    <ul>
+                      <li>
+                        <a href="/UsuariosBlendsAdmin" >
+                          Usuarios de blends
+                        </a>
+                      </li>
+                      <li>
+                      <a href="/PagoMembresiaAdmin" id="settings-select" >
+                          Pagos de Membresia (Premium)
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className=" row amigos admins-space">
+              <div className="friend">
+                    <div className="friend_title">
+                        <img src="images/user-7.jpg" alt="" />
+                        <span><b>Pedro Espinoza</b><p>4 Friends in Common</p></span>
+                        <button className="add-friend">Agregar</button>
+                    </div>
+                    <div className="friend_title">
+                        <img src="images/user-2.jpg" alt="" />
+                        <span><b>Maria Martinez</b><p>2 Friends in Common</p></span>
+                        <button className="add-friend">Agregar</button>
+                    </div>
+                    <div className="friend_title">
+                        <img src="images/user-3.jpg" alt="" />
+                        <span><b>Andres Pineda</b><p>1 Friends in Common</p></span>
+                        <button className="add-friend">Agregar</button>
+                    </div>
+                    <div className="friend_title">
+                        <img src="images/user-6.jpg" alt="" />
+                        <span><b>Laura Hernandez</b><p>4 Friends in Common</p></span>
+                        <button className="delete-friend">Eliminar</button>
+                    </div>
+                    <div className="friend_title">
+                        <img src="images/user-4.jpg" alt="" />
+                        <span><b>Tony Stevens</b><p>Mutual Friend: Sarah Hetfield</p></span>
+                        <button className="delete-friend">Eliminar</button>
+                    </div>
+
+
+
+                </div>
+              </div>
+
+              </div>
+            </div>
+              
+            
+          </section>
+          {/*------------------------ SECCION LATERAL DERECHA---------------*/}
+        </section>
+      </div>
+    </>
+  );
+}
